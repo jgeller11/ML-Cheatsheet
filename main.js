dcbeadd00b148db418071c3aa025cef1bdf7070f
jgeller11 <34071775+jgeller11@users.noreply.github.com>
2020-04-24 15:59:13 -0400

    hnnggggggg

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function hide(num){
-  if(document.getElementById('box'+num.toString()).style.display==='none'){
-    document.getElementById('box'+num.toString()).style.display='inline'
+  if(document.getElementById('box'+num.toString()).style.display==='inline'){
+    document.getElementById('box'+num.toString()).style.display='none'
     document.getElementById('button'+num.toString()).style.background='lightgrey'
   } else {
-    document.getElementById('box'+num.toString()).style.display='none'
+    document.getElementById('box'+num.toString()).style.display='inline'
     document.getElementById('button'+num.toString()).style.background='grey'
   }
 }
